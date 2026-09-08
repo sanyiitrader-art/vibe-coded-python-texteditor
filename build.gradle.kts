c23878516ad069d2336c438c1de3b618507dc365
sanyiitrader-art <sanyiitrader@gmail.com>
2026-09-08 20:24:02 +0300

    first try

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file using traditional buildscript to ensure Chaquopy resolves correctly
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://chaquo.com/maven")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.chaquo.python:gradle:15.0.1")
+    }
+}
+
+// Clean up old plugins block if it exists
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.chaquo.python") version "15.0.1" apply false
 }
